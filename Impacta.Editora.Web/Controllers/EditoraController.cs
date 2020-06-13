3946fe56cf4c367d0338905b5589cc2ddd3e129c
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-13 12:03:14 -0300

    Construção do cadastro da editora.

---
+++ Impacta.Editora.Web/Controllers/EditoraController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Impacta.Editora.Web.Controllers
+{
+    public class EditoraController : Controller
+    {
+        // GET: Editora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CadastrarEditora()
+        {
+            return View();
+        }
+    }
+}

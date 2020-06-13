e2126a65071ae078a65dd17158152078a0bd24ed
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-13 11:17:53 -0300

    Add project files.

---
+++ Impacta.Editora.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Impacta.Editora.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Impacta.Editora.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Impacta.Editora.Web.MvcApplication" Language="C#" %>

e4e414a932d5581e2dbfdefba6592910a6600f4d
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 12:15:45 -0300

    Remover as ActionResult de Contato e Sobre

---
+++ Impacta.Editora.Web/Controllers/InicioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Impacta.Editora.Web.Controllers
+{
+    public class InicioController : Controller
+    {
+        //Por default todos os métodos do controller respondem pelo verbo GT do Http
+        //[HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

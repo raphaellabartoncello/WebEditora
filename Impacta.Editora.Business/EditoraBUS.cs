216ccb2c559c67ef6982bdcb300973f93467b24a
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 13:08:43 -0300

    Creiar camada Businss e implantar validação de dados para chamada no DB

---
+++ Impacta.Editora.Business/EditoraBUS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Impacta.Editora.DataADO;
+
+namespace Impacta.Editora.Business
+{
+    public class EditoraBUS
+    {
+        EditoraDATA objDataADO = null;
+
+        public bool Cadastrar(Model.Editora editora)
+        {
+            objDataADO = new EditoraDATA();
+
+            return objDataADO.Save(editora);
+        }
+
+
+    }
+}

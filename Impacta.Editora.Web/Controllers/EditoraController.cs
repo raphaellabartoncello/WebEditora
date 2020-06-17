10915d50aeb30c1debfe6598735cafd1a8a8e134
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 13:12:52 -0300

    ActionResult de cadastro de editora

--- Impacta.Editora.Web/Controllers/EditoraController.cs
+++ Impacta.Editora.Web/Controllers/EditoraController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Impacta.Editora.Business;
 using System.Web.Mvc;
 
 namespace Impacta.Editora.Web.Controllers
@@ -17,6 +14,32 @@ namespace Impacta.Editora.Web.Controllers
         public ActionResult CadastrarEditora()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarEditora(Model.Editora editora)
+        {
+            //objeto BUS
+            EditoraBUS editoraBUS = new EditoraBUS();
+
+            if (!editoraBUS.Cadastrar(editora))
+            {
+                //A ViewBag é uma estrutura dinâmica para criação de propriedades, assim é possível enviar informações da Controller para View - A ViewBag se mantém na memória durante no máximo um POST
+                ViewBag.ResultadoCadastro = "O cadastro da editora não foi realizado.";
+
+                return View(editora);
+            }
+
+            ViewBag.ResultadoCadastroSucesso = "O cadastro da editora foi realizado com sucesso.";
+
+            //ViewData[string key] é uma estrutura dinâmica mas ao invés de criar propriedade (como a ViewBag) ela fornece m identificador, chamados de chave (key) para armazenar o valor - A ViewData se mantém na memória durante no máximo um POST (um redirecionamento)
+            ViewData["ResultadoCadastro"] = "Cadastro realizado com sucesso";
+
+            //TempData consegue se manter na memória por mais de um redirecionamento
+            TempData["NovaMensagem"] = "Você incluiu um novo cadastro!";
+
+            return View();
+
         }
     }
 }

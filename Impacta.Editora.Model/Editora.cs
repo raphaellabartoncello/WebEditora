b8af6ad1299746b7c5e2cbb9652c28bb43b87b64
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 12:46:39 -0300

    Criada Model da Editora para leitura, manipulação e validação de dados

---
+++ Impacta.Editora.Model/Editora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Impacta.Editora.Model
+{
+    public class Editora
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Nome { get; set; }
+
+        public int Telefone { get; set; }
+
+        public int Email { get; set; }
+
+        public int Observacoes { get; set; }
+
+
+
+    }
+}

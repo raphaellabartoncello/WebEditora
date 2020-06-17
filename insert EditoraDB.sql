1c31cd09ba434cbdcbe5ad95f07ef62237b7149f
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 19:18:54 -0300

    Popular banco de dados para validação da lista de editoras

---
+++ insert EditoraDB.sql
@@ -0,0 +1,34 @@
+﻿insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 1', '11966667777', 'contato@editora1.com.br', 'Contrato de faturamento para 30 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 2', '11966667777', 'contato@editora2.com.br', 'Contrato de faturamento para 15 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 3', '11966667777', 'contato@editora3.com.br', 'Contrato de faturamento mensal');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 4', '11966667777', 'contato@editora4.com.br', 'Contrato de faturamento para 15 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 5', '11966667777', 'contato@editora5.com.br', 'Contrato de faturamento para 15 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 6', '11966667777', 'contato@editora6.com.br', 'Contrato de faturamento para 10 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 7', '11966667777', 'contato@editora7.com.br', 'Contrato de faturamento para 30 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 8', '11966667777', 'contato@editora8.com.br', 'Contrato de faturamento para 10 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 9', '11966667777', 'contato@editora9.com.br', 'Contrato de faturamento para 30 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 10', '11966667777', 'contato@editora10.com.br', 'Contrato de faturamento para 10 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 11', '11966667777', 'contato@editora11.com.br', 'Contrato de faturamento mensal');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 12', '11966667777', 'contato@editora12.com.br', 'Contrato de faturamento para 05 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 13', '11966667777', 'contato@editora13.com.br', 'Contrato de faturamento para 05 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 14', '11966667777', 'contato@editora14.com.br', 'Contrato de faturamento mensal');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 15', '11966667777', 'contato@editora15.com.br', 'Contrato de faturamento para 30 dias após a compra');
+insert into Editora (NomeEditora, Telefone, Email, Observacoes)
+values('Editora 16', '11966667777', 'contato@editora16.com.br', 'Contrato de faturamento mensal');
+
+select * from Editora
